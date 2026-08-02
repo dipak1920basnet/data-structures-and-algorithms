96645079267cd0d454b564c7ef20f471f0378331
dipak1920basnet <dipak1920basnet@gmail.com>
2026-08-02 12:11:13 +0545

    build a tail recursion

--- recursion/build_recursion.py
+++ recursion/build_recursion.py
@@ -1,10 +1,23 @@
 count = 0
-def func():
+def head_func():
     global count
     if count == 4:
         return
-    print("Hello world")
+    print("Head recursion")
     count += 1
-    func()
-func()
-print(count)
+    head_func()
+head_func()
+print()
+
+
+new_count = 0
+
+def tail_func():
+    global new_count
+    if new_count == 4:
+        return
+    new_count += 1
+    tail_func()
+    print("Tail recursion")
+
+tail_func()
