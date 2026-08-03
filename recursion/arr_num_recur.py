3d8fc6c78f347ff1765877dbdaa0412bd058bd79
dipak1920basnet <dipak1920basnet@gmail.com>
2026-08-03 21:34:01 +0545

    reverse a string using while loop too

--- recursion/arr_num_recur.py
+++ recursion/arr_num_recur.py
@@ -4,13 +4,41 @@ def recur_arr(arr, left, right):
     # temp_left = arr[left]
     # arr[left] = arr[right]
     # arr[right] = temp_left
+    # left += 1
+    # right -= 1
 
     arr[left], arr[right] = arr[right], arr[left]
-
-    left += 1
-    right -= 1
-    return recur_arr(arr, left, right)
+    return recur_arr(arr, left+1, right-1)
 
 arr = [0,1,2,3,4,5,6,7,8,9]
 
-print(recur_arr(arr, 2, 6))
+
+
+
+# reverse an arry using while loop
+
+array = [0,1,2,3,4,5,6,7,8,9]
+
+def while_reverse(arr):
+    # totally reverse a array
+    left = 0
+    right = len(arr) - 1
+
+    while left<=  right:
+        arr[left], arr[right] = arr[right], arr[left]
+        left +=1
+        right -= 1
+    return arr
+
+
+
+def while_reverse_parameter(arr, left, right):
+    while left<=  right:
+        arr[left], arr[right] = arr[right], arr[left]
+        left +=1
+        right -= 1
+    return arr
+
+print(recur_arr(array.copy(), 2, 6))
+print(while_reverse_parameter(array.copy(), 2, 6))
+print(while_reverse(array.copy()))
