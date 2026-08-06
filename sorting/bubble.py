e6d1dbc6f339d511f78481a2566f122940edef72
dipak1920basnet <dipak1920basnet@gmail.com>
2026-08-06 11:40:24 +0545

    write an algo to perform bubble sort

---
+++ sorting/bubble.py
@@ -0,0 +1,44 @@
+def Bubble_sort_asc(n):
+    if len(n) == 0 or len(n) == 1:
+        return n
+    i = 1
+    while True:
+        if n[i] < n[i-1]:
+            n[i-1], n[i] = n[i], n[i-1]
+        i+=1
+        if i >= len(n):
+            i = 1
+
+        for j in range(1,len(n)):
+            if n[j] < n[j-1]:
+                break
+        else:
+            break
+    return n 
+
+def Bubble_sort_dsc(n):
+    if len(n) == 0 or len(n) == 1:
+        return n
+    i = 1
+    while True:
+        if n[i] > n[i-1]:
+            n[i-1], n[i] = n[i], n[i-1]
+        i+=1
+        if i >= len(n):
+            i = 1
+
+        for j in range(1,len(n)):
+            if n[j] > n[j-1]:
+                break
+        else:
+            break
+    return n 
+
+def Bubble_sort(n, reverse:bool = False):
+    if reverse:
+        return Bubble_sort_dsc(n)
+    return Bubble_sort_asc(n)
+
+n = [5,8,1,6,9,2,4]
+n = [2,1]
+print(Bubble_sort(n))
