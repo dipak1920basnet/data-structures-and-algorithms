339a9728a8320b82655ee8e6cf374c06321255e9
dipak1920basnet <dipak1920basnet@gmail.com>
2026-08-07 20:45:50 +0545

    build a insertation sort

--- sorting/insertation.py
+++ sorting/insertation.py
@@ -19,12 +19,36 @@ n =  [5,8,1,6,9,2,4]
 # Insertation(new_n)
 # print(new_n==[1, 2, 5, 8, 10, 12, 14, 23, 31, 34, 38, 45, 54, 56, 67, 76, 78, 89, 90, 99])
 
-# Empty list
-Insertation([])
+# # Empty list
+# Insertation([])
 
-# Single element
-Insertation([5])
+# # Single element
+# Insertation([5])
 
-# Already sorted
-Insertation([1, 2, 3, 4, 5])
+# # Already sorted
+# Insertation([1, 2, 3, 4, 5])
             
+
+
+def insertation_sort(n):
+    for i in range(1,len(n)):
+        if n[i] < n[i-1]:
+            temp = n[i]
+            n[i] = n[i-1]
+            for j in range(i-1, -1,-1):
+                if j == 0:
+                    n[j] = temp
+                    break
+                elif n[j-1] > temp:
+                    n[j] = n[j-1]
+                else:
+                    n[j] = temp
+                    break
+
+    print(n)
+
+insertation_sort([1, 2, 3, 4, 5])
+insertation_sort([5])
+insertation_sort([])
+insertation_sort([34, 12, 78, 56, 23, 89, 1, 45, 67, 10, 99, 2, 31, 54, 76, 8, 14, 90, 5, 38])
+
