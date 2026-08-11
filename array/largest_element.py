893290959de0e7688334cca0a404661951aaae4e
dipak1920basnet <dipak1920basnet@gmail.com>
2026-08-11 10:22:39 +0545

    find the second largest with single for loop

--- array/largest_element.py
+++ array/largest_element.py
@@ -1,12 +1,18 @@
 def find_largest(n:list[int|float])->int|float|None:
     # python treats empty list as false
     if not n:
+        # return None if the list is empty
         return None
     largest = n[0]
     # largest = float("-inf")
     for i in n:
+
+        # check if current number is greater than available largest number
+        # if yes change the largest number to current number
         if i > largest:
             largest = i
+
+        # largest = max(i, largest)
     return largest
 
 nums = [55,32,-97,99.99,3,67]
--- array/second_largest_element.py
+++ array/second_largest_element.py
@@ -15,5 +15,23 @@ def find_second_largest(n:list[int|float])-> None| float| int:
 
     return second_largest
 
-nums = [55,32,97,-55,45,32,88,21]
-print(find_second_largest(nums))
+
+# approach 2:
+def second_largest(n:list[int|float])-> None| float| int:
+    stack = [n[0],float("-inf")]
+    for i in n:
+        # if another largest exists then set (second largest to largest and largest to new largest)
+        if i > stack[0]:
+            stack[0], stack[1] = i, stack[0]
+
+        # if i is greater then second largest and smaller than largest then set second largest to i
+        elif i > stack[1] and i < stack[0]:
+            stack[1] = i
+    return stack[1]
+
+
+
+nums = [55,32,97,-55,45,32,88,21,97]
+# print(find_second_largest(nums))
+print(second_largest(nums))
+
