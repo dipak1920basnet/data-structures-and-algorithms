c883e15581272c7ccbd1e473dde9baf955bd85e5
dipak1920basnet <dipak1920basnet@gmail.com>
2026-08-16 15:22:21 +0545

    merge without duplicates

--- array/merge.py
+++ array/merge.py
@@ -29,3 +29,41 @@ def Merge(arr:list, arr_2:list) -> list:
 arr   = [1,2,3,4,6,7]
 arr_2 = [1,2,3,4,6,7,8,9] 
 print(Merge(arr, arr_2))
+
+
+
+def merge_no_duplicate(arr:list, arr_2:list) -> list:
+    i = 0
+    j = 0
+
+    arrays = []
+
+    while i < len(arr) and j < len(arr_2):
+        if arr[i] < arr_2[j]:
+            # check if duplicate exists in first array
+            if i == 0 or arrays[-1] != arr[i]:
+                arrays.append(arr[i])
+            i += 1
+        elif arr[i] > arr_2[j]:
+            # check if duplicate exists in second array
+            if j == 0 or arrays[-1] != arr_2[j]:
+                arrays.append(arr_2[j])
+            j += 1
+        else:
+            arrays.append(arr[i])
+            i += 1
+            j += 1
+
+    while j < len(arr_2):
+        if arr_2[j] != arrays[-1]:
+            arrays.append(arr_2[j])
+        j +=1
+    
+    while i < len(arr):
+        if arr[i] != arr[-1]:
+            arrays.append(arr[i])
+        i +=1
+            
+    return arrays
+
+print(merge_no_duplicate(arr, arr_2))
