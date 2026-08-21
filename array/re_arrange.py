61094768477484e336dad577035d159590e4bd88
dipak1920basnet <dipak1920basnet@gmail.com>
2026-08-21 08:52:47 +0545

    optimal solution for rearraning the array in positive and negative alternate order

--- array/re_arrange.py
+++ array/re_arrange.py
@@ -1,36 +1,38 @@
 # Approach one
-def rearrangeArray(self, nums: list[int]) -> list[int]:
-    positive_stack = []
-    negative_stack = []
+# def rearrangeArray(nums: list[int]) -> list[int]:
+#     positive_stack = []
+#     negative_stack = []
 
-    for i in nums:
-        if i >= 0:
-            positive_stack.append(i)
-        else:
-            negative_stack.append(i)
+#     for i in nums:
+#         if i >= 0:
+#             positive_stack.append(i)
+#         else:
+#             negative_stack.append(i)
 
-    final_stack = []
+#     final_stack = []
 
-    for j in range(len(positive_stack)):
-        final_stack.append(positive_stack[j])
-        final_stack.append(negative_stack[j])
+#     for j in range(len(positive_stack)):
+#         final_stack.append(positive_stack[j])
+#         final_stack.append(negative_stack[j])
 
-    return final_stack
+#     return final_stack
 
 
 # Approach two:
 
-# def RearrangeArray(self, nums: list[int]) -> list[int]:
-#     array = []
-#     positive_tracer = 0
-#     negative_tracer = 1
+def RearrangeArray(nums: list[int]) -> list[int]:
+    array = [0]*len(nums)
+    pos = 0
+    neg = 1
+    for i in range(len(array)):
+        if nums[i] >= 0:
+            array[pos] = nums[i]
+            pos += 2
+        else:
+            array[neg] = nums[i]
+            neg += 2
+    print(array)
 
-#     while positive_tracer < len(nums) and negative_tracer < len(nums):
-#         ...
+nums = [3,1,-2,-5,2,-4]
 
-
-
-
-# nums = [3,1,-2,-5,2,-4]
-
-# RearrangeArray(nums)
+RearrangeArray(nums)
